375ed39d23b9004a6d6c71bba63807920cebc33f
Colin Faivre <colin.faivre@gmail.com>
2022-09-22 12:06:05 +0200

    build: :wrench: fix nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules: ['@pinia/nuxt'],
+    modules: ['@pinia/nuxt'],
     css: [
         'vuetify/lib/styles/main.sass',
         'mdi/css/materialdesignicons.min.css',
