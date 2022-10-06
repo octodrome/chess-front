862a60fc516d9c38275fff01a10fa9d21b576202
Colin Faivre <colin.faivre@gmail.com>
2022-10-06 09:56:37 +0200

    fix: get connected user from app.vue

--- app.vue
+++ app.vue
@@ -1,8 +1,28 @@
 <script>
 import { useSnackbarStore } from "~/stores/snackbarStore";
 import { mapState, mapActions } from "pinia";
+import { useComputerGameStore } from "./stores/computerGameStore";
+import { useUserStore } from "./stores/userStore";
+import { useHumanGameStore } from "./stores/humanGameStore";
 
 export default {
+  mounted() {
+    const LocalStorage = process.client ? localStorage : null;
+
+    function parseToken(token) {
+      return JSON.parse(atob(token.split(".")[1]));
+    }
+    const userToken = LocalStorage.getItem("token");
+
+    if (userToken) {
+      const userId = parseToken(userToken).userId;
+      this.getUser(userId);
+      this.getUserGames(userId);
+    }
+
+    this.getGames();
+  },
+
   computed: {
     ...mapState(useSnackbarStore, [
       "showSnackbar",
@@ -24,6 +44,9 @@ export default {
 
   methods: {
     ...mapActions(useSnackbarStore, ["hide"]),
+    ...mapActions(useUserStore, ["getUser"]),
+    ...mapActions(useHumanGameStore, ["getUserGames"]),
+    ...mapActions(useComputerGameStore, ["getGames"]),
   },
 };
 </script>
