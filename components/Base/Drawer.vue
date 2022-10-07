4e6fefad627c3559958e2097ad3eeda92443ee3d
Colin Faivre <colin.faivre@gmail.com>
2022-10-07 14:53:15 +0200

    fix: :bug: increase drawer z-index to fix modal display

--- components/Base/Drawer.vue
+++ components/Base/Drawer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-800 text-gray-200 basis-80 shrink-0 drop-shadow-lg">
+  <div class="bg-gray-800 text-gray-200 basis-80 shrink-0 drop-shadow-lg z-20">
     <slot />
   </div>
 </template>
