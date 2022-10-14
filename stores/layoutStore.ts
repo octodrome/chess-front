c24814d594b361c7a756fa8e47e0b9627fe6589b
Colin Faivre <colin.faivre@gmail.com>
2022-10-14 11:31:26 +0200

    refactor: remove unused actions and simplify layoutStore

--- stores/layoutStore.ts
+++ stores/layoutStore.ts
@@ -1,40 +1,16 @@
 export const useLayoutStore = defineStore('layout', {
   state: () => ({
-    drawerLeftIsOpened: false,
-    drawerRightIsOpened: false,
+      drawerLeftIsOpened: false,
+      drawerRightIsOpened: false,
   }),
 
   actions: {
     toggleLeftDrawer() {
-      this.TOGGLE_LEFT_DRAWER();
+      this.drawerLeftIsOpened = !this.drawerLeftIsOpened;
     },
 
     toggleRightDrawer() {
-      this.TOGGLE_RIGHT_DRAWER();
-    },
-
-    setDrawerLeft(value: boolean) {
-      this.SET_DRAWER_LEFT(value);
-    },
-
-    setDrawerRight(value: boolean) {
-      this.SET_DRAWER_RIGHT(value);
-    },
-
-    TOGGLE_LEFT_DRAWER() {
-      this.drawerLeftIsOpened = !this.drawerLeftIsOpened;
-    },
-    
-    TOGGLE_RIGHT_DRAWER() {
       this.drawerRightIsOpened = !this.drawerRightIsOpened;
-    },
-    
-    SET_DRAWER_LEFT(value) {
-      this.drawerLeftIsOpened = value;
-    },
-    
-    SET_DRAWER_RIGHT(value) {
-      this.drawerRightIsOpened = value;
     },
   },
 })
